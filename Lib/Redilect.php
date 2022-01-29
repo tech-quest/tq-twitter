27ac35b1d70aadee642ce74c14f66bca2273edfd
upa178 <u.chanchan0910@gmail.com>
2022-01-29 11:38:00 +0900

    リダイレクト処理をRedirectクラスにしました。

---
+++ Lib/Redilect.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * リダイレクトを扱うクラス
+ */
+final class Redirect
+{
+    /**
+     * 実行関数
+     * 
+     * @param string $path リダイレクト先のパス
+     */
+    public static function handler(string $path): void
+    {
+        header('Location: ' . $path);
+        die;
+    }
+}
