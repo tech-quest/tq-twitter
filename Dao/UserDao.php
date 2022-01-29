ca37227b1900f7aaeb205c70e28039bf28a81041
upa178 <u.chanchan0910@gmail.com>
2022-01-29 11:37:29 +0900

    ログイン処理のDaoを作成し、Daoディレクトリにまとめました。

---
+++ Dao/UserDao.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/Dao.php';
+require_once __DIR__ . '/../ValueObject/Email.php';
+
+final class UserDao extends Dao
+{
+    public function findByEmail(string $email): ?array
+    {
+        $sql = "
+        SELECT 
+            *
+        FROM 
+            users
+        WHERE
+            email = :email";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $emailObject = new Email($email);
+        $stmt->bindValue(':email', $emailObject->value(), PDO::PARAM_STR);
+
+        $res = $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return (empty($user)) ? null : $user;
+        if (!$res) {
+            throw new Exception('ユーザー情報の登録に失敗しました');
+        }
+    }
+}
