fb1d23f02328eac17d169cfdca692a04011d5cfc
upa178 <u.chanchan0910@gmail.com>
2022-01-29 11:38:18 +0900

    Emailのバリューオブジェクトを作成しました。

---
+++ ValueObject/Email.php
@@ -0,0 +1,16 @@
+<?php
+
+final class Email
+{
+    public function __construct(string $value)
+    {
+        if (empty($value)) throw new Exception("値を入力してください");
+
+        $this->value = $value;
+    }
+
+    public function value(): string
+    {
+        return $this->value;
+    }
+}
