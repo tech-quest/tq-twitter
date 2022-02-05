7190123a93864e33ea3c5dca8314c3e959d924d3
mitsuhiro <papu.m0917@icloud.com>
2022-02-05 18:39:12 +0900

    スペース削除

--- src/public/tweetComplete.php
+++ src/public/tweetComplete.php
@@ -8,7 +8,6 @@ $user_id = 1;
 $tweet = filter_input(INPUT_POST, 'tweet');
 $replyTweetId = 1;
 $device = 'iphone';
-
 $tweetsDao = new TweetDao();
 $tweetsDao->insert($user_id, $tweet, $replyTweetId, $device);
 header('Location: /index.php');
