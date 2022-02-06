79f16d04d5b372fa13515692ee340e24485aefde
mitsuhiro <papu.m0917@icloud.com>
2022-02-06 23:35:04 +0900

    ツイート日時、デバイス名を表示できるようにした

--- src/public/index.php
+++ src/public/index.php
@@ -6,9 +6,9 @@ use App\Dao\TweetDao;
 
 $tweetDao = new TweetDao();
 $tweets = $tweetDao->findByAllTweets();
+$device = $_SERVER['HTTP_USER_AGENT'];
 ?>
 <!DOCTYPE html>
-
 <head>
     <title>Topページ</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -20,11 +20,16 @@ $tweets = $tweetDao->findByAllTweets();
             <h1>Topページ</h1>
             <form method="post" action="tweetComplete.php">
                 <input type="text" name="tweet" required placeholder="いまなにしてる?" />
+                <input type="hidden" name="device" value="<?php echo $device; ?>" />
                 <input type="submit" value="ツイートする" />
             </form>
             <?php foreach ($tweets as $tweet): ?>
                 <p><?php echo $tweet['tweet']; ?></p>
-                <p><?php echo $tweet['created_at']; ?></p>
+                <p><?php echo $tweet['created_at'] .
+                    ' ' .
+                    'Twitter for' .
+                    ' ' .
+                    $tweet['device']; ?></p>
             <?php endforeach; ?>
         </div>
     </main>
