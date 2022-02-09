2e05317b4e28395a9d2c50ed393654fba1ff941c
mitsuhiro <papu.m0917@icloud.com>
2022-02-10 01:55:26 +0900

    user_idの仮の値を変更した

--- src/public/tweetComplete.php
+++ src/public/tweetComplete.php
@@ -6,7 +6,7 @@ use App\Dao\TweetDao;
 use App\Lib\Redirect;
 use App\Lib\Device;
 
-$user_id = 1;
+$user_id = 2;
 $tweet = filter_input(INPUT_POST, 'tweet');
 $replyTweetId = 1;
 $deviceInfo = filter_input(INPUT_POST, 'device');
