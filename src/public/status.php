6dd836c593db105f171786aa1f295eae76311a5e
mitsuhiro <papu.m0917@icloud.com>
2022-02-14 22:51:46 +0900

    feat: ツイート関連のボタンを追加

--- src/public/status.php
+++ src/public/status.php
@@ -12,10 +12,30 @@ $tweetDao = new TweetDao();
 $tweet = $tweetDao->findById($tweetId, $user_Id);
 $tweetDate = new TweetDate($tweet['created_at']);
 ?>
-<h1>ツイート詳細ページ</h1> 
-<p><?php echo $tweet['tweet']; ?></p>
-<p><?php echo $tweetDate->date() .
-    '・' .
-    'Twitter for' .
-    ' ' .
-    $tweet['device']; ?></p>
+<!DOCTYPE html>
+<head>
+  <title>ツイート詳細ページ</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body id="status">
+  <main>
+    <div class="container">
+      <h1>ツイート詳細ページ</h1> 
+      <div class="tweet-status">
+        <p class="tweet-status_tweet"><?php echo $tweet['tweet']; ?></p>
+        <p class="tweet-status_date"><?php echo $tweetDate->date() .
+            '・' .
+            'Twitter for' .
+            ' ' .
+            $tweet['device']; ?></p>
+      </div>
+      <div class="tweet-button">
+        <a href="">Reply</a>
+        <a href="">Retweet</a>
+        <a href="">Like</a>
+        <a href="">Share</a>
+      </div>
+    </div>
+  </main>
+</body>
+</html>
