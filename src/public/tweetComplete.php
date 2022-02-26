90203f34e0b1b20cb4efe03c2759fb9daeb6145a
mitsuhiro <papu.m0917@icloud.com>
2022-02-26 16:22:05 +0900

    refactor: ディレクトリの修正

--- src/public/tweetComplete.php
+++ src/public/tweetComplete.php
@@ -2,13 +2,13 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Dao\TweetDao;
+use App\Infrastructure\Dao\TweetDao;
 use App\Lib\Redirect;
 use App\Domain\ValueObject\Device;
 
 $user_id = 2;
 $tweet = filter_input(INPUT_POST, 'tweet');
-$replyTweetId = 1;
+$replyTweetId = 4;
 $deviceInfo = filter_input(INPUT_POST, 'device');
 
 $device = new Device($deviceInfo);
