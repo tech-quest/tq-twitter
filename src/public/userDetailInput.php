e5ef6622ca3bf5e2ce5dccbb424635cee9333aa3
mitsuhiro <papu.m0917@icloud.com>
2022-03-07 23:45:55 +0900

    refactor: レイアウトの変更

--- src/public/userDetailInput.php
+++ src/public/userDetailInput.php
@@ -11,17 +11,21 @@ unset($_SESSION['errors']);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>userDetailInput</title>
 </head>
 
 <body>
+  <div class="user-detail">
     <p><?php echo $error[0]; ?></p>
-    <h1>パスワードの変更確認</h1>
-    <form action="passwordReset.php" method="post">
-        <p><input type="text" name="name" placeholder="名前"></p>
-        <p><input type="text" name="email" placeholder="Email"></p>
-        <p><input type="submit" value="次へ"></p>
-    </form>
+    <div class="user-detail__input">
+      <h1>Twitterアカウントを探す</h1>
+      <form action="passwordReset.php" method="post">
+          <input class="user-info" type="text" name="name" placeholder="メールアドレスか電話番号かユーザー名を入力してください">
+          <input class="user-search" type="submit" value="検索">
+      </form>
+    </div>
+  </div>
 </body>
 
 </html>
