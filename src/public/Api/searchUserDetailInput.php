d2e864db09b0321c4dfe2ea61f33fbade3a85fa4
mitsuhiro <papu.m0917@icloud.com>
2022-03-20 19:20:52 +0900

    feat: searchUserDetailInputの作成

---
+++ src/public/Api/searchUserDetailInput.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use App\Infrastructure\Dao\UserDao;
+
+date_default_timezone_set('Asia/Tokyo');
+header('Content-Type: application/json; charset=UTF-8'); //ヘッダー情報の明記。必須。
+/**
+ * POST通信でもグローバル変数「$_POST」からは値を参照できない点に注意してください。
+ * その代わり、「php://input」より受け取ったデータを参照することができます。
+ */
+$searchInput = json_decode(file_get_contents('php://input'), true);
+
+$userDao = new UserDao();
+$findByEmail = $userDao->findByEmail($searchInput['input']);
+
+$status = [
+    'data' => [
+        'email' => $findByEmail['email'],
+    ],
+];
+echo json_encode($status);
+die();
