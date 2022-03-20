f1d52420d0585e4ed66ec99f9ac0869f3a1a9bc7
mitsuhiro <papu.m0917@icloud.com>
2022-03-20 18:36:01 +0900

    refactor: インデント修正

--- src/public/signin.php
+++ src/public/signin.php
@@ -8,23 +8,23 @@ unset($_SESSION['errors']);
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 
 <body>
-    <?php foreach ($errors as $error): ?>
-        <p><?php echo $error; ?></p>
-    <?php endforeach; ?>
-    <h1>Twitterにログイン</h1>
-    <form action="signin-complete.php" method="post">
-        <p><input type="text" name="email" placeholder="Email"></p>
-        <p><input type="password" name="password" placeholder="password"></p>
-        <p><input type="submit" value="次へ"></p>
-        <a href="userDetailInput.php">パスワードを忘れた場合はこちら</a>
-    </form>
+  <?php foreach ($errors as $error): ?>
+    <p><?php echo $error; ?></p>
+  <?php endforeach; ?>
+  <h1>Twitterにログイン</h1>
+  <form action="signin-complete.php" method="post">
+    <p><input type="text" name="email" placeholder="Email"></p>
+    <p><input type="password" name="password" placeholder="password"></p>
+    <p><input type="submit" value="次へ"></p>
+    <a href="userDetailInput.php">パスワードを忘れた場合はこちら</a>
+  </form>
 
 </body>
 
