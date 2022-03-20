5e1d7884c0f68f027aaf8e5a89c0c14967e47b4d
mitsuhiro <papu.m0917@icloud.com>
2022-03-20 19:15:03 +0900

    feat: confirmCertificationCodeInputの作成

---
+++ src/public/Api/confirmUserCertificationCodeInput.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use App\Adapter\QueryService\CertificationCodeQueryService;
+use App\Lib\Session;
+use App\Domain\ValueObject\UserId;
+
+$session = Session::getInstance();
+$email = $_SESSION['certificate_email'];
+date_default_timezone_set('Asia/Tokyo');
+// header('Content-Type: application/json; charset=UTF-8'); //ヘッダー情報の明記。必須。
+/**
+ * POST通信でもグローバル変数「$_POST」からは値を参照できない点に注意してください。
+ * その代わり、「php://input」より受け取ったデータを参照することができます。
+ */
+$certificationCode = json_decode(file_get_contents('php://input'), true);
+$emailCertificationCode = $email . $certificationCode['code'];
+$hashEmailCertificationCode = hash('sha3-512', $emailCertificationCode);
+$queryService = new CertificationCodeQueryService();
+$findByCertificationCode = $queryService->findByCertificationCode(
+    $hashEmailCertificationCode
+);
+$session->setUserId(new UserId($findByCertificationCode['user_id']));
+
+$status = [
+    'data' => [
+        'userId' => $findByCertificationCode['user_id'],
+        'certificationCode' => $findByCertificationCode['certification_code'],
+    ],
+];
+echo json_encode($status);
+die();
