d4b16f73fef5444e097bd129593146718e56c210
mitsuhiro <papu.m0917@icloud.com>
2022-03-20 18:39:37 +0900

    refactor: インデント修正

--- src/public/userDetailInput.php
+++ src/public/userDetailInput.php
@@ -8,24 +8,26 @@ unset($_SESSION['errors']);
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>userDetailInput</title>
+  <meta charset="UTF-8">
+  <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>userDetailInput</title>
 </head>
 
 <body>
-  <div class="user-detail">
-    <p><?php echo $error[0]; ?></p>
-    <div class="user-detail__input">
-      <h1>Twitterアカウントを探す</h1>
-      <form action="passwordReset.php" method="post">
-          <input class="user-info" type="text" name="name" placeholder="メールアドレスか電話番号かユーザー名を入力してください">
-          <input class="user-search" type="submit" value="検索">
-      </form>
-    </div>
-  </div>
+  <div>
+    <div class="user-detail">
+      <p><?php echo $errors[0]; ?></p>
+      <div class="user-detail__input">
+        <h1>Twitterアカウントを探す</h1>
+        <form action="" method="post">
+          <div class="field">
+            <label for="email">メールアドレスか電話番号かユーザー名を入力してください</label>
+            <input class="user-info input" type="text" name="name" />
+          </div>
+          <input class="user-search" type="submit" value="検索" disabled />
+        </form>
 </body>
 
 </html>
