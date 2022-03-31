505d3e1c9bbc28cb6ec46bbfbe1f59693bcfb68d
upa178 <u.chanchan0910@gmail.com>
2022-03-31 14:59:05 +0900

    feat: shareボタンにクラスを追加して、表示箇所を変更しました。

--- src/public/status.php
+++ src/public/status.php
@@ -13,47 +13,49 @@ $output = $useCase->handler();
 $tweet = $output->tweet();
 ?>
 <!DOCTYPE html>
+
 <head>
   <title>ツイート詳細ページ</title>
   <link rel="stylesheet" href="style.css">
 </head>
+
 <body id="status">
   <main>
     <div class="container">
-      <button type=“button” onclick="location.href='../index.php'">←</button>
-      <h1>ツイート詳細ページ</h1> 
+      <h1>ツイート詳細ページ</h1>
       <div class="tweet-status">
         <p class="tweet-status__tweet"><?php echo $tweet
-            ->tweetBody()
-            ->value(); ?></p>
+                                          ->tweetBody()
+                                          ->value(); ?></p>
         <p class="tweet-status__date"><?php echo $tweet->createdAt()->date() .
-            '・' .
-            'Twitter for' .
-            ' ' .
-            $tweet->device()->value(); ?></p>
+                                        '・' .
+                                        'Twitter for' .
+                                        ' ' .
+                                        $tweet->device()->value(); ?></p>
       </div>
       <div class="tweet-button">
-        <a href="">Reply</a>
-        <a href="">Retweet</a>
-        <a href="">Like</a>
-        <a href="" class="modalopen">Share</a>
-      </div>
-      <div class="modal">
-        <a href="">ダイレクトメッセージで送信</a>
-        <a href="">ブックマーク</a>
-        <a href="">リンク</a> 
+        <span class="share-link">Reply</span>
+        <span class="share-link">Retweet</span>
+        <span class="share-link">Like</span>
+        <div class="share-button-wrapper">
+          <span class="modalopen share-link">Share</span>
+          <div class="modal">
+            <div class="share-link">ダイレクトメッセージで送信</div>
+            <div class="share-link">ブックマーク</div>
+            <div class="share-link">リンク</div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
 </body>
+
 </html>
 
 <script>
   const buttonOpen = document.querySelector('.modalopen');
   const modal = document.querySelector('.modal');
   buttonOpen.addEventListener('click', function(e) {
-    e.preventDefault();
-    modal.classList.add('active');
-    overlay.classList.add('active');
+    modal.classList.toggle('active');
   });
 </script>
