0e5f129548d2dc22b0efc520893abf923b2a6bba
mitsuhiro <papu.m0917@icloud.com>
2022-04-05 21:58:44 +0900

    refactor: クラスの変更

--- src/public/Api/confirmUserCertificationCodeInput.php
+++ src/public/Api/confirmUserCertificationCodeInput.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use App\Adapter\QueryService\CertificationCodeQueryService;
+use App\Infrastructure\Dao\CertificationCodeDao;
 use App\Lib\Session;
 use App\Domain\ValueObject\UserId;
 
@@ -16,16 +16,16 @@ date_default_timezone_set('Asia/Tokyo');
 $certificationCode = json_decode(file_get_contents('php://input'), true);
 $emailCertificationCode = $email . $certificationCode['code'];
 $hashEmailCertificationCode = hash('sha3-512', $emailCertificationCode);
-$queryService = new CertificationCodeQueryService();
-$findByCertificationCode = $queryService->findByCertificationCode(
+$certificationCodeDao = new CertificationCodeDao();
+$userCertificationCode = $certificationCodeDao->findByCertificationCode(
     $hashEmailCertificationCode
 );
-$session->setUserId(new UserId($findByCertificationCode['user_id']));
+$session->setUserId(new UserId($userCertificationCode['user_id']));
 
 $status = [
     'data' => [
-        'userId' => $findByCertificationCode['user_id'],
-        'certificationCode' => $findByCertificationCode['certification_code'],
+        'userId' => $userCertificationCode['user_id'],
+        'certificationCode' => $userCertificationCode['certification_code'],
     ],
 ];
 echo json_encode($status);
