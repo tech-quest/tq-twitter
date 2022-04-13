9c39f0c8506690b38b1d756adcc8dd6ebf2de66d
mitsuhiro <papu.m0917@icloud.com>
2022-04-13 22:39:06 +0900

    feat: UserQueryServiceの作成

---
+++ src/app/Adapter/QueryService/UserQueryService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Adapter\QueryService;
+
+use App\Domain\Entity\User;
+use App\Infrastructure\Dao\UserDao;
+use App\Domain\ValueObject\UserId;
+use App\Domain\ValueObject\Name;
+use App\Domain\ValueObject\Email;
+use App\Domain\ValueObject\Password;
+
+final class UserQueryService
+{
+  private UserDao $userDao;
+
+  public function __construct()
+  {
+    $this->userDao = new UserDao();
+  }
+
+  public function findById(UserId $id): User
+  {
+    $userMapper = $this->userDao->findById($id->value());
+
+    return new User(
+      new UserId($userMapper['id']),
+      new Name($userMapper['name']),
+      new Email($userMapper['email']),
+      new Password($userMapper['password'])
+    );
+  }
+}
