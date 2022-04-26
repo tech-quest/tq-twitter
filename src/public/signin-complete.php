587cb4eb3f1534a76e31bdca8065803a1f45d163
mitsuhiro <papu.m0917@icloud.com>
2022-04-26 17:42:12 +0900

    refactor: リダイレクト先の変更

--- src/public/signin-complete.php
+++ src/public/signin-complete.php
@@ -42,7 +42,7 @@ try {
 
     $session->setAuth($authUser);
 
-    Redirect::handler('/profile.php');
+    Redirect::handler('/index.php');
 } catch (Exception $e) {
     echo $e->getMessage();
     die();
