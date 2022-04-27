424b0a2eb92d09f02a067f2c81acdd9ed5c85383
mitsuhiro <papu.m0917@icloud.com>
2022-04-27 23:06:57 +0900

    refactor: jsの記述を別ファイルに切り分けた

--- src/public/index.php
+++ src/public/index.php
@@ -11,7 +11,7 @@ $session = Session::getInstance();
 $authUser = $session->auth();
 
 if (is_null($authUser)) {
-    Redirect::handler('/signin.php');
+  Redirect::handler('/signin.php');
 }
 
 $tweetDao = new TweetDao();
@@ -76,7 +76,7 @@ $session->setDevice(new Device($device));
             <input type="hidden" name="device" value="<?php echo $device; ?>" />
             <input type="submit" value="ツイートする" />
           </form>
-          <?php foreach ($tweets as $tweet): ?>
+          <?php foreach ($tweets as $tweet) : ?>
             <a href="status.php?id=<?php echo $tweet['id']; ?>">
               <p><?php echo $tweet['tweet']; ?></p>
             </a>
@@ -107,73 +107,4 @@ $session->setDevice(new Device($device));
 </body>
 
 </html>
-
-<script>
-  const tweetButton = document.querySelector('.tweet-button');
-  tweetButton.addEventListener('click', async function(event) {
-    event.preventDefault();
-    const tweetModal = document.querySelector('.tweet-modal');
-    tweetModal.classList.add('show-modal');
-  }, false);
-</script>
-
-<script>
-  const sendTweet = document.querySelector('.send-tweet');
-  sendTweet.addEventListener('click', async function(event) {
-    event.preventDefault();
-    const tweetInput = document.querySelector('.tweet');
-    const tweet = tweetInput.value;
-    const obj = {
-      tweet,
-    };
-    const body = JSON.stringify(obj);
-    const headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    };
-    const response = await fetch(
-      'Api/complete-user-tweet.php', {
-        method: "POST",
-        headers,
-        body
-      });
-    const json = await response.json();
-    if (json.data.status) {
-      const tweetModal = document.querySelector('.tweet-modal');
-      tweetModal.classList.remove('show-modal');
-      window.location.reload();
-    }
-  }, false);
-</script>
-
-<script>
-  const inputValue = document.getElementById("inputForm")
-  inputValue.addEventListener('keyup', (event) => {
-    const input = inputValue.value;
-    console.log(input);
-    if (input.length >= 140) {
-      inputValue.classList.add('color');
-      const sendTweet = document.querySelector('.send-tweet');
-      sendTweet.classList.add('hover');
-      sendTweet.disabled = true;
-    }
-    if (input.length <= 140) {
-      inputValue.classList.remove('color');
-      const sendTweet = document.querySelector('.send-tweet');
-      sendTweet.classList.remove('hover');
-      sendTweet.disabled = false;
-    }
-  }, false);
-</script>
-
-<script>
-  const tweetModal = document.querySelector('.tweet-modal');
-  tweetModal.addEventListener('click', (event) => {
-    if (!tweetModal.classList.contains('show-modal')) {
-      return;
-    }
-    if (event.target === event.target.closest('.tweet-modal')) {
-      tweetModal.classList.remove('show-modal');
-    }
-  }, false);
-</script>
+<script src="tweet-modal.js"></script>
