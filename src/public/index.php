c446d268181e646ead1ca62b4a2fe4a2b1c5384d
mitsuhiro <papu.m0917@icloud.com>
2022-04-29 13:31:24 +0900

    refactor: require_onceの追加

--- src/public/index.php
+++ src/public/index.php
@@ -31,43 +31,7 @@ $session->setDevice(new Device($device));
 <body>
   <div class="wrapper">
     <div class="content">
-      <div class="left-bar">
-        <nav class="nav flex-column">
-          <a class="nav-link active" aria-current="page" href="#">
-            <img src="" alt="" width="30" height="30" class="">
-            ホーム
-          </a>
-          <a class="nav-link" href="#">
-            <img src="" alt="" width="30" height="30" class="">
-            話題を検索
-          </a>
-          <a class="nav-link" href="#">
-            <img src="" alt="" width="30" height="30" class="">
-            通知
-          </a>
-          <a class="nav-link" href="#">
-            <img src="" alt="" width="30" height="30" class="">
-            メッセージ
-          </a>
-          <a class="nav-link" href="#">
-            <img src="" alt="" width="30" height="30" class="">
-            ブックマーク
-          </a>
-          <a class="nav-link" href="#">
-            <img src="" alt="" width="30" height="30" class="">
-            リスト
-          </a>
-          <a class="nav-link" href="#">
-            <img src="" alt="" width="30" height="30" class="">
-            プロフィール
-          </a>
-          <a class="nav-link" href="#">
-            <img src="" alt="" width="30" height="30" class="">
-            もっと見る
-          </a>
-          <input class="radius-pixel-20 btn btn-primary tweet-button" type="submit" value="ツイートする">
-        </nav>
-      </div>
+      <?php require_once __DIR__ . '/left_nav.php'; ?>
       <main>
         <div class="container">
           <h1>Topページ</h1>
