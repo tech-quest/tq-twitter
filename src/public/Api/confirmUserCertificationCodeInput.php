4a2c3ca60a7156bfa9f27ff91f699ea767f42278
mitsuhiro <papu.m0917@icloud.com>
2022-05-07 13:02:09 +0900

    refactor: $sessionに関連する記述の修正

--- src/public/Api/confirmUserCertificationCodeInput.php
+++ src/public/Api/confirmUserCertificationCodeInput.php
@@ -6,7 +6,7 @@ use App\Lib\Session;
 use App\Domain\ValueObject\UserId;
 
 $session = Session::getInstance();
-$email = $_SESSION['certificate_email'];
+$email = $session->certificateEmail();
 date_default_timezone_set('Asia/Tokyo');
 // header('Content-Type: application/json; charset=UTF-8'); //ヘッダー情報の明記。必須。
 /**
@@ -14,7 +14,7 @@ date_default_timezone_set('Asia/Tokyo');
  * その代わり、「php://input」より受け取ったデータを参照することができます。
  */
 $certificationCode = json_decode(file_get_contents('php://input'), true);
-$emailCertificationCode = $email . $certificationCode['code'];
+$emailCertificationCode = $email->value() . $certificationCode['code'];
 $hashEmailCertificationCode = hash('sha3-512', $emailCertificationCode);
 $certificationCodeDao = new CertificationCodeDao();
 $userCertificationCode = $certificationCodeDao->findByCertificationCode(
