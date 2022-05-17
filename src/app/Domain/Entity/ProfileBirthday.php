093b7070700cf60bda7baeb9d24fba6a5c03504c
upa178 <u.chanchan0910@gmail.com>
2022-05-17 14:59:39 +0900

    add: 今回は使用していませんが、プロフィール登録時のValueObjectを作成しました。

---
+++ src/app/Domain/Entity/ProfileBirthday.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Domain\Entity;
+
+use App\Domain\ValueObject\UserId;
+use App\Domain\ValueObject\Birthday;
+
+final class ProfileBirthday
+{
+  private $userId;
+  private $birthday;
+
+  public function __construct(
+    UserId $userId,
+    Birthday $birthday
+  ) {
+    $this->userId = $userId;
+    $this->birthday = $birthday;
+  }
+
+  public function id(): UserId
+  {
+    return $this->userId;
+  }
+}
