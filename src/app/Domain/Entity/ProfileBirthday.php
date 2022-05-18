15614b5a695dfcac046090b367a6d54981069be2
upa178 <u.chanchan0910@gmail.com>
2022-05-18 15:51:50 +0900

    feta: 今回は使用していませんが、登録時のバリデーションを作成しました。

--- src/app/Domain/Entity/ProfileBirthday.php
+++ src/app/Domain/Entity/ProfileBirthday.php
@@ -7,19 +7,17 @@ use App\Domain\ValueObject\Birthday;
 
 final class ProfileBirthday
 {
-  private $userId;
-  private $birthday;
+    private $userId;
+    private $birthday;
 
-  public function __construct(
-    UserId $userId,
-    Birthday $birthday
-  ) {
-    $this->userId = $userId;
-    $this->birthday = $birthday;
-  }
+    public function __construct(UserId $userId, Birthday $birthday)
+    {
+        $this->userId = $userId;
+        $this->birthday = $birthday;
+    }
 
-  public function id(): UserId
-  {
-    return $this->userId;
-  }
+    public function id(): UserId
+    {
+        return $this->userId;
+    }
 }
