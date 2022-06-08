4b1549e3fe6b620d87989e66b7778942b5963c2d
mitsuhiro <papu.m0917@icloud.com>
2022-06-08 21:25:17 +0900

    refactor: Password::isValidメソッドの実装

--- src/app/Domain/ValueObject/Password.php
+++ src/app/Domain/ValueObject/Password.php
@@ -6,6 +6,7 @@ use Exception;
 
 final class Password
 {
+    private const PATTERN =  '/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,24}+\z/';
     private $value;
 
     public function __construct(string $value)
@@ -21,5 +22,10 @@ final class Password
     public function hashAsString(): string
     {
         return password_hash($this->value, PASSWORD_DEFAULT);
+    }
+
+    public static function isValid(string $password): bool
+    {
+        return preg_match(self::PATTERN, $password);
     }
 }
