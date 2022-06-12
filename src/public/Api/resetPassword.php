67dca81e52a755883abea4c8fa45515c1d0a6359
mitsuhiro <papu.m0917@icloud.com>
2022-06-12 16:49:58 +0900

    refactor: 不要な記述の削除

--- src/public/Api/resetPassword.php
+++ src/public/Api/resetPassword.php
@@ -1,11 +1,8 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-use App\Infrastructure\Dao\UserDao;
-use App\Infrastructure\Dao\CertificationCodeDao;
 use App\Lib\Session;
 use App\Domain\ValueObject\Password;
-use App\Infrastructure\Validator\PasswordInputValidator;
 use App\Domain\ValueObject\UserId;
 use App\Domain\ValueObject\Email;
 use App\UseCase\PasswordReset\CompletePasswordReset\Input;
@@ -13,7 +10,6 @@ use App\UseCase\PasswordReset\CompletePasswordReset\Interactor;
 
 $session = Session::getInstance();
 date_default_timezone_set('Asia/Tokyo');
-// header('Content-Type: application/json; charset=UTF-8');
 $inputs = json_decode(file_get_contents('php://input'), true);
 $id = (int) $_SESSION['user_id'];
 $email = $_SESSION['email'];
--- src/public/Api/sendMail.php
+++ src/public/Api/sendMail.php
@@ -8,9 +8,6 @@ use App\UseCase\PasswordReset\SendResetPasswordCertification\Interactor;
 use Dotenv\Dotenv;
 use App\Lib\Session;
 use App\Domain\ValueObject\Email;
-use App\Infrastructure\Dao\UserDao;
-use App\Infrastructure\Dao\CertificationCodeDao;
-use App\UseCase\PasswordReset\PasswordCertificationSender;
 
 $session = Session::getInstance();
 Dotenv::createImmutable(__DIR__ . '/../../')->load();
@@ -21,32 +18,3 @@ $email = new Email($inputs['email']);
 $useCaseInput = new Input($email);
 $useCaseInteractor = new Interactor($useCaseInput);
 $useCaseInteractor->handler();
-
-// $userDao = new UserDao();
-// $user = $userDao->findByEmail($inputs['email']);
-
-// $certificationCode = chr(mt_rand(97, 122));
-// for ($i = 0; $i < 10; $i++) {
-//     $certificationCode .= chr(mt_rand(97, 122));
-// }
-
-// $emailCertificationCode = $user['email'] . $certificationCode;
-// $hashCertificationCode = hash('sha3-512', $emailCertificationCode);
-// $certificationCodeDao = new CertificationCodeDao();
-// $certificationCodeDao->insertPasswordCertification(
-//     $user['id'],
-//     $hashCertificationCode
-// );
-
-// $session->setCertificateEmail(new Email($user['email']));
-// $session->setUserEmail(new Email($user['email']));
-
-// try {
-//     $passwordCertificationSender = new PasswordCertificationSender(
-//         $user,
-//         $certificationCode
-//     );
-//     $passwordCertificationSender->send();
-// } catch (Exception $e) {
-//     echo 'error:' . $e->getMessage();
-// }
