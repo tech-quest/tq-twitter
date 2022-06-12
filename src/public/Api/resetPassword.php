ea01ebb9f48f15a30677e5c9a756fd698a877e09
mitsuhiro <papu.m0917@icloud.com>
2022-06-12 16:43:22 +0900

    refactor: パスワード変更完了の処理をUseCaseにまとめた

--- src/public/Api/resetPassword.php
+++ src/public/Api/resetPassword.php
@@ -6,36 +6,29 @@ use App\Infrastructure\Dao\CertificationCodeDao;
 use App\Lib\Session;
 use App\Domain\ValueObject\Password;
 use App\Infrastructure\Validator\PasswordInputValidator;
+use App\Domain\ValueObject\UserId;
+use App\Domain\ValueObject\Email;
+use App\UseCase\PasswordReset\CompletePasswordReset\Input;
+use App\UseCase\PasswordReset\CompletePasswordReset\Interactor;
 
 $session = Session::getInstance();
 date_default_timezone_set('Asia/Tokyo');
-header('Content-Type: application/json; charset=UTF-8');
+// header('Content-Type: application/json; charset=UTF-8');
 $inputs = json_decode(file_get_contents('php://input'), true);
-$id = $_SESSION['user_id'];
+$id = (int) $_SESSION['user_id'];
 $email = $_SESSION['email'];
+
 $password = new Password($inputs['newPassword']);
-
-$passwordInputError = new PasswordInputValidator($password->value());
-if (!is_null($passwordInputError->passwordErrorText())) {
-    $response = [
-        'data' => [
-            'status' => false,
-            'message' => '不正な形式のパスワードです'
-        ],
-    ];
-    echo json_encode($response);
-    die;
-}
-
-
-$user = new UserDao();
-$newPassword = $user->updatePassword($id, $password->hashAsString());
-$certificationCodeDao = new CertificationCodeDao();
-$result = $certificationCodeDao->deleteByCertificationCode($id);
+$userId = new UserId($id);
+$userEmail = new Email($email);
+$useCaseInput = new Input($password, $userId, $userEmail);
+$useCaseInteractor = new Interactor($useCaseInput);
+$useCaseOutput = $useCaseInteractor->handler();
 
 $response = [
     'data' => [
-        'status' => $result,
+        'status' => $useCaseOutput->isSuccess(),
+        'message' => $useCaseOutput->message(),
     ],
 ];
 
