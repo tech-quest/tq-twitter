85145fb9f33c4f8f37b5a49f174724c495e8abe9
mitsuhiro <papu.m0917@icloud.com>
2022-06-16 11:13:23 +0900

    feat: right-bar.phpの作成

--- src/public/right-bar.php
+++ src/public/right-bar.php
@@ -8,84 +8,98 @@ use App\Lib\Session;
 $session = Session::getInstance();
 $authUser = $session->auth();
 
-// if (is_null($authUser)) {
-//   Redirect::handler('/signin.php');
-// }
+if (is_null($authUser)) {
+    Redirect::handler('/signin.php');
+}
 ?>
 <!DOCTYPE html>
 <div class="sidebar">
     <div class="sidebar-search">
         <input type="text" placeholder="キーワード検索">
     </div>
-    <div class=" sidebar-trend">
-        <h2>いまどうしてる？</h2>
+    <div class="sidebar-trend">
+        <span class="top-text">いまどうしてる？</span>
         <div class="sidebar-trend__box">
             <div class="sidebar-trend__box-top">
-                <span>ニュース・ライブ</span>
+                <span class="title">ニュース・ライブ</span>
             </div>
             <div class="sidebar-trend__box-middle">
-                <span>テキストテキストテキストテキスト</span>
-            </div>
-            <div class="sidebar-trend__box-bottom">
-                トレンドトピック:
-                <a href="#">
-                    <span>テキストテキストテキストテキスト</span>
-                </a>
+                <span class="trend">テキストテキストテキストテキスト</span>
             </div>
         </div>
         <div class="sidebar-trend__box">
             <div class="sidebar-trend__box-top">
-                <span>テクノロジー・トレンド</span>
+                <span class="title">テクノロジー・トレンド</span>
             </div>
             <div class="sidebar-trend__box-middle">
-                <span>テキストテキストテキストテキスト</span>
+                <span class="trend">テキストテキストテキストテキスト</span>
             </div>
-            <div class="sidebar-trend__box-bottom">
-                トレンドトピック:
-                <a href="#">
-                    <span>テキストテキストテキストテキスト</span>
-                </a>
-            </div>
+
         </div>
         <div class="sidebar-trend__box">
             <div class="sidebar-trend__box-top">
-                <span>日本のトレンド</span>
+                <span class="title">日本のトレンド</span>
             </div>
             <div class="sidebar-trend__box-middle">
-                <span>テキストテキストテキストテキスト</span>
+                <span class="trend">テキストテキストテキストテキスト</span>
             </div>
-            <div class="sidebar-trend__box-bottom">
-                トレンドトピック:
-                <a href="#">
-                    <span>テキストテキストテキストテキスト</span>
-                </a>
-            </div>
+
         </div>
         <div class="sidebar-trend__box">
             <div class="sidebar-trend__box-top">
-                <span>国際ニュース・ライブ</span>
+                <span class="title">国際ニュース・ライブ</span>
             </div>
             <div class="sidebar-trend__box-middle">
-                <span>テキストテキストテキストテキスト</span>
+                <span class="trend">テキストテキストテキストテキスト</span>
             </div>
-            <div class="sidebar-trend__box-bottom">
-                トレンドトピック:
-                <a href="#">
-                    <span>テキストテキストテキストテキスト</span>
-                </a>
-            </div>
+
         </div>
     </div>
     <div class="sidebar-user">
+        <span class="top-text">おすすめユーザー</span>
         <div class="sidebar-user__wrapper">
             <div class="sidebar-user__icon">
                 <img src="./image/twittericon13.jpeg"></img>
             </div>
-            <div class="sidebar-user__name">
-                <span>ユーザー1</span>
+            <div class="sidebar-user__wrapper-account">
+                <div class="sidebar-user__name">
+                    <span>ユーザー1</span>
+                </div>
+                <div class="sidebar-user__id">
+                    <span>@ユーザー1</span>
+                </div>
             </div>
-            <div class="sidebar-user__id">
-                <span>@ユーザー1</span>
+            <div class="sidebar-user__button">
+                <span>フォロー</span>
+            </div>
+        </div>
+        <div class="sidebar-user__wrapper">
+            <div class="sidebar-user__icon">
+                <img src="./image/twittericon13.jpeg"></img>
+            </div>
+            <div class="sidebar-user__wrapper-account">
+                <div class="sidebar-user__name">
+                    <span>ユーザー2</span>
+                </div>
+                <div class="sidebar-user__id">
+                    <span>@ユーザー2</span>
+                </div>
+            </div>
+            <div class="sidebar-user__button">
+                <span>フォロー</span>
+            </div>
+        </div>
+        <div class="sidebar-user__wrapper">
+            <div class="sidebar-user__icon">
+                <img src="./image/twittericon13.jpeg"></img>
+            </div>
+            <div class="sidebar-user__wrapper-account">
+                <div class="sidebar-user__name">
+                    <span>ユーザー3</span>
+                </div>
+                <div class="sidebar-user__id">
+                    <span>@ユーザー3</span>
+                </div>
             </div>
             <div class="sidebar-user__button">
                 <span>フォロー</span>
