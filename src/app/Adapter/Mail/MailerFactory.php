c4d2e6a7f873f2ceaeb06798e4152bd5d56a74a1
mitsuhiro <papu.m0917@icloud.com>
2022-06-22 20:56:18 +0900

    refactor: 不要なコメントの削除

--- src/app/Adapter/Mail/MailerFactory.php
+++ src/app/Adapter/Mail/MailerFactory.php
@@ -4,15 +4,11 @@ namespace App\Adapter\Mail;
 
 use App\Domain\Adapter\Mailer;
 use App\Infrastructure\Mail\MailTrap;
-use App\Infrastructure\Mail\Gmail;
 
 final class MailerFactory
 {
-  public static function create(string $subject, string $body): Mailer
-  {
-    // if ($_ENV['env'] === 'production') {
-    //   return new Gmail($subject, $body);
-    // }
-    return new MailTrap($subject, $body);
-  }
+    public static function create(string $subject, string $body): Mailer
+    {
+        return new MailTrap($subject, $body);
+    }
 }
