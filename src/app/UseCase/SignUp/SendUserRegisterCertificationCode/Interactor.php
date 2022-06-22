df78625944b275b785c5d02562eb864f360db214
mitsuhiro <papu.m0917@icloud.com>
2022-06-22 20:32:18 +0900

    refactor: インデントの修正

--- src/app/UseCase/SignUp/SendUserRegisterCertificationCode/Interactor.php
+++ src/app/UseCase/SignUp/SendUserRegisterCertificationCode/Interactor.php
@@ -13,58 +13,58 @@ use App\Domain\ValueObject\Certification;
 
 final class Interactor
 {
-  private const REGISTERD_MESSAGE = 'メールアドレスが登録されています。';
-  private const NOT_REGISTERD_MESSAGE = 'まだ登録されていないメールアドレスです。';
-  private Input $input;
+    private const REGISTERD_MESSAGE = 'メールアドレスが登録されています。';
+    private const NOT_REGISTERD_MESSAGE = 'まだ登録されていないメールアドレスです。';
+    private Input $input;
 
-  public function __construct(Input $input)
-  {
-    $this->userDao = new UserDao();
-    $this->input = $input;
-  }
-
-  public function handler(): Output
-  {
-    if ($this->existsUserByEmail()) {
-      return new Output(false, self::REGISTERD_MESSAGE);
+    public function __construct(Input $input)
+    {
+        $this->userDao = new UserDao();
+        $this->input = $input;
     }
 
-    $signUpCertificationCode = new Certification($this->input->email());
-    $this->insertRegisterCertification($signUpCertificationCode);
-    $this->saveUserInfo($signUpCertificationCode);
-    $this->sendCertificationCodeMail($signUpCertificationCode);
+    public function handler(): Output
+    {
+        if ($this->existsUserByEmail()) {
+            return new Output(false, self::REGISTERD_MESSAGE);
+        }
 
-    return new Output(true, self::NOT_REGISTERD_MESSAGE);
-  }
+        $signUpCertificationCode = new Certification($this->input->email());
+        $this->insertRegisterCertification($signUpCertificationCode);
+        $this->saveUserInfo($signUpCertificationCode);
+        $this->sendCertificationCodeMail($signUpCertificationCode);
 
-  private function saveUserInfo(Certification $Certification): void
-  {
-    $session = Session::getInstance();
-    $session->setRegisterCertificateEmail($this->input->email());
-    $session->setUserName($this->input->name());
-    $session->setHashCertificateEmail($Certification->generateHash());
-  }
-
-  private function sendCertificationCodeMail(Certification $Certification)
-  {
-    try {
-      $signUpCertificationSender = new SignUpCertificationSender($Certification->code());
-      return $signUpCertificationSender->send();
-    } catch (Exception $e) {
-      return 'error:' . $e->getMessage();
+        return new Output(true, self::NOT_REGISTERD_MESSAGE);
     }
-  }
 
-  private function insertRegisterCertification(Certification $Certification): void
-  {
-    $userRegisterCodeDao = new UserRegisterCertificationCodeDao();
-    $userRegisterCodeDao->insertRegisterCertification($Certification->generateHash());
-  }
+    private function saveUserInfo(Certification $Certification): void
+    {
+        $session = Session::getInstance();
+        $session->setRegisterCertificateEmail($this->input->email());
+        $session->setUserName($this->input->name());
+        $session->setHashCertificateEmail($Certification->generateHash());
+    }
 
-  private function existsUserByEmail(): bool
-  {
-    $email = $this->input->email();
-    $user = $this->userDao->findByEmail($email->value());
-    return !is_null($user);
-  }
+    private function sendCertificationCodeMail(Certification $Certification)
+    {
+        try {
+            $signUpCertificationSender = new SignUpCertificationSender($Certification->code());
+            return $signUpCertificationSender->send();
+        } catch (Exception $e) {
+            return 'error:' . $e->getMessage();
+        }
+    }
+
+    private function insertRegisterCertification(Certification $Certification): void
+    {
+        $userRegisterCodeDao = new UserRegisterCertificationCodeDao();
+        $userRegisterCodeDao->insertRegisterCertification($Certification->generateHash());
+    }
+
+    private function existsUserByEmail(): bool
+    {
+        $email = $this->input->email();
+        $user = $this->userDao->findByEmail($email->value());
+        return !is_null($user);
+    }
 }
