e87249df21afa5b6090c6cbac168595cfc3789d4
mitsuhiro <papu.m0917@icloud.com>
2022-07-20 20:34:58 +0900

    refactor: TweetBodyの修正とテストの追加

---
+++ src/test/TweetBodyTest.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Domain\ValueObject\TweetBody;
+use PHPUnit\Framework\TestCase;
+
+final class TweetBodyTest extends TestCase
+{
+    /** @test */
+    public function ツイートが1文字の時_エラーが発生しないこと()
+    {
+        $expected = 'a';
+        $tweetBody = new TweetBody($expected);
+        $actual = $tweetBody->value();
+        $this->assertSame($expected, $actual);
+    }
+
+    /** @test */
+    public function ツイートが140文字の時_エラーが発生しないこと()
+    {
+        $expected = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+        $tweetBody = new TweetBody($expected);
+        $actual = $tweetBody->value();
+        $this->assertSame($expected, $actual);
+    }
+
+    /** @test */
+    public function ツイートが0文字の時_エラーが発生すること()
+    {
+        $this->expectException(Exception::class);
+        new TweetBody('');
+    }
+
+    /** @test */
+    public function ツイートが141字の時_エラーが発生すること()
+    {
+        $this->expectException(Exception::class);
+        new TweetBody(
+            'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        );
+    }
+}
